e3fa7f021b8ee1e47c94908bd7bb26d41df8771d
jonkee <zzzgoo@gmail.com>
2020-01-12 00:15:07 +0800

    fetch support customized headers

--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,12 @@
 const nodeFetch = require('node-fetch')
 
-let fetch = function (url) {
+let fetch = function (url, header_obj={}) {
 	let pathName = url
+
 	return nodeFetch(url, {
-		headers: {
+		headers: Object.assign({
 			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36',
-		},
+		}, header_obj),
 		redirect: 'error',
 	}).then(resp => {
 		if (resp.ok) {
